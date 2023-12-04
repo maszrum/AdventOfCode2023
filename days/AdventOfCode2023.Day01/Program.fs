82f1db523fc37ae4546f8865874151532ddca4f5
maszrum <grzyb3k@hotmail.com>
2023-12-04 21:21:28 +0100

    Days 01 and 02

---
+++ days/AdventOfCode2023.Day01/Program.fs
@@ -0,0 +1,103 @@
+﻿open System
+open System.IO
+
+// part 1
+
+let readInputLines (streamReader: StreamReader) =
+    seq {
+        while not streamReader.EndOfStream do
+            yield streamReader.ReadLine()
+    }
+
+let findFirstDigit input =
+    input
+    |> Seq.tryFind Char.IsDigit
+    |> Option.map (fun character -> int(character - '0'))
+    |> Option.defaultValue 0
+
+let findLastDigit input =
+    input
+    |> Seq.rev
+    |> findFirstDigit
+
+let createNumberFromTwoDigits (d1, d2) =
+    (d1 * 10) + d2
+
+let resultPartOne =
+    use fileStream = File.OpenRead("input.txt")
+    use streamReader = new StreamReader(fileStream)
+
+    readInputLines streamReader
+    |> Seq.map(fun line -> (findFirstDigit line, findLastDigit line))
+    |> Seq.map createNumberFromTwoDigits
+    |> Seq.sum
+
+printfn $"Part 1 result: %d{resultPartOne}"
+
+// part 2
+
+let textualDigitToNumberLength3 (input: string, fromEnd: bool) =
+    if input.Length < 3 then
+        None
+    else
+        let substring = if fromEnd then input.Substring(input.Length - 3) else input.Substring(0, 3);
+        match substring with
+        | "one" -> Some 1
+        | "two" -> Some 2
+        | "six" -> Some 6
+        | _ -> None
+
+let textualDigitToNumberLength4 (input: string, fromEnd: bool) =
+    if input.Length < 4 then
+        None
+    else
+        let substring = if fromEnd then input.Substring(input.Length - 4) else input.Substring(0, 4)
+        match substring with
+        | "four" -> Some 4
+        | "five" -> Some 5
+        | "nine" -> Some 9
+        | _ -> None
+
+let textualDigitToNumberLength5 (input: string, fromEnd: bool) =
+    if input.Length < 5 then
+        None
+    else
+        let substring = if fromEnd then input.Substring(input.Length - 5) else input.Substring(0, 5);
+        match substring with
+        | "three" -> Some 3
+        | "seven" -> Some 7
+        | "eight" -> Some 8
+        | _ -> None
+
+let digitToNumber (input: string, fromEnd: bool) =
+    let character = if fromEnd then input[input.Length - 1] else input[0];
+    match Char.IsDigit(character) with
+    | true -> Some (int(character - '0'))
+    | false -> None
+
+let rec readDigitFromString (input: string, fromEnd: bool) =
+    match digitToNumber(input, fromEnd) with
+    | Some value -> value
+    | None ->
+        match textualDigitToNumberLength3(input, fromEnd) with
+        | Some value -> value
+        | None ->
+            match textualDigitToNumberLength4(input, fromEnd) with
+            | Some value -> value
+            | None ->
+                match textualDigitToNumberLength5(input, fromEnd) with
+                | Some value -> value
+                | None ->
+                    let substring = if fromEnd then input.Substring(0, input.Length - 1) else input.Substring(1)
+                    readDigitFromString(substring, fromEnd)
+
+let resultPartTwo =
+    use fileStream = File.OpenRead("input.txt")
+    use streamReader = new StreamReader(fileStream)
+
+    readInputLines streamReader
+    |> Seq.map(fun line -> (readDigitFromString(line, false), readDigitFromString(line, true)))
+    |> Seq.map(createNumberFromTwoDigits)
+    |> Seq.sum
+
+printfn $"Part 2 result: %d{resultPartTwo}"
---
+++ days/AdventOfCode2023.Day02/Program.fs
@@ -0,0 +1,113 @@
+﻿open System
+open System.IO
+
+// part 1
+
+type ShownCubes =
+    { Red: Option<int>
+      Green: Option<int>
+      Blue: Option<int> }
+
+type Game =
+    { Number: int
+      ShownCubes: ShownCubes[] }
+
+let readInputLines (streamReader: StreamReader) =
+    seq {
+        while not streamReader.EndOfStream do
+            yield streamReader.ReadLine()
+    }
+
+let parseInputLine (line: string) =
+    let indexOfColon = line.IndexOf(':')
+    let gameNumber = Int32.Parse(line.Substring(5, indexOfColon - 5))
+    let shownCubesSerialized = line.Substring(indexOfColon + 2)
+
+    let shownCubes =
+        shownCubesSerialized.Split(';', StringSplitOptions.RemoveEmptyEntries + StringSplitOptions.TrimEntries)
+        |> Seq.map (fun set -> set.Split(',', StringSplitOptions.RemoveEmptyEntries + StringSplitOptions.TrimEntries))
+        |> Seq.map (fun set ->
+            let parseElement (acc: ShownCubes) (value: string) =
+                let indexOfSpace = value.IndexOf(' ')
+                let number = Int32.Parse(value.Substring(0, indexOfSpace))
+
+                if value.EndsWith("red") then
+                    { acc with Red = Some(number) }
+                else if value.EndsWith("green") then
+                    { acc with Green = Some(number) }
+                else if value.EndsWith("blue") then
+                    { acc with Blue = Some(number) }
+                else
+                    failwith "Invalid input"
+
+            let initialAccumulator =
+                { Red = None
+                  Green = None
+                  Blue = None }
+
+            Seq.scan parseElement initialAccumulator set |> Seq.last)
+        |> Seq.toArray
+
+    { Number = gameNumber
+      ShownCubes = shownCubes }
+
+let isGamePossible (game: Game) =
+    let isNoneOrLEThan (value: Option<int>, leThan: int) =
+        match value with
+        | None -> true
+        | Some value -> value <= leThan
+
+    game.ShownCubes
+    |> Seq.forall (fun cubes ->
+        isNoneOrLEThan (cubes.Red, 12)
+        && isNoneOrLEThan (cubes.Green, 13)
+        && isNoneOrLEThan (cubes.Blue, 14))
+
+let parsedGames =
+    use fileStream = File.OpenRead("input.txt")
+    use streamReader = new StreamReader(fileStream)
+    readInputLines streamReader |> Seq.map parseInputLine |> Seq.toArray
+
+let resultPartOne =
+    parsedGames |> Seq.filter isGamePossible |> Seq.sumBy (fun game -> game.Number)
+
+printfn $"Part 1 result: %d{resultPartOne}"
+
+// part 2
+
+let createBiggestPossibleGame (game: Game) =
+    let initialAccumulator =
+        { Red = Some(0)
+          Green = Some(0)
+          Blue = Some(0) }
+
+    let createBiggest (acc: ShownCubes) (value: ShownCubes) =
+        let acc =
+            if (defaultArg value.Red 0) > acc.Red.Value then
+                { acc with Red = value.Red }
+            else
+                acc
+
+        let acc =
+            if (defaultArg value.Green 0) > acc.Green.Value then
+                { acc with Green = value.Green }
+            else
+                acc
+
+        let acc =
+            if (defaultArg value.Blue 0) > acc.Blue.Value then
+                { acc with Blue = value.Blue }
+            else
+                acc
+
+        acc
+
+    Seq.scan createBiggest initialAccumulator game.ShownCubes |> Seq.last
+
+let resultPartTwo =
+    parsedGames
+    |> Seq.map createBiggestPossibleGame
+    |> Seq.map (fun game -> (defaultArg game.Red 0) * (defaultArg game.Green 0) * (defaultArg game.Blue 0))
+    |> Seq.sum
+
+printfn $"Part 2 result: %d{resultPartTwo}"
